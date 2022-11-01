bc179abe9e195b7599be3c506e48b33a0af26d4d
Apexuh <mrapexuh@gmail.com>
2022-11-01 12:36:06 +0300

    add guessing_game.py and training task 1.5

---
+++ Stepik/guessing_game.py
@@ -0,0 +1,114 @@
+import random
+
+word_list = ['fun','globus', 'world']
+
+
+def get_word(lst):
+    return random.choice([i.upper() for i in lst])
+
+# функция получения текущего состояния
+def display_hangman(tries):
+    stages = [  # финальное состояние: голова, торс, обе руки, обе ноги
+                '''
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / \\
+                   -
+                ''',
+                # голова, торс, обе руки, одна нога
+                '''
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / 
+                   -
+                ''',
+                # голова, торс, обе руки
+                '''
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |      
+                   -
+                ''',
+                # голова, торс и одна рука
+                '''
+                   --------
+                   |      |
+                   |      O
+                   |     \\|
+                   |      |
+                   |     
+                   -
+                ''',
+                # голова и торс
+                '''
+                   --------
+                   |      |
+                   |      O
+                   |      |
+                   |      |
+                   |     
+                   -
+                ''',
+                # голова
+                '''
+                   --------
+                   |      |
+                   |      O
+                   |    
+                   |      
+                   |     
+                   -
+                ''',
+                # начальное состояние
+                '''
+                   --------
+                   |      |
+                   |      
+                   |    
+                   |      
+                   |     
+                   -
+                '''
+    ]
+    return stages[tries]
+
+def test_symbol(word):
+    if len(word) == 0:
+        print('Enter something!')
+        return False
+    for i in word:
+        if not isinstance(i, str):
+            print('Enter some symbol please')
+            return False
+    return True
+
+
+
+def play(word):
+    print('Давайте играть в угадайку слов!')
+    word_completion = '_' * len(word)  # строка, содержащая символы _ на каждую букву задуманного слова
+    guessed = False  # сигнальная метка
+    guessed_letters = []  # список уже названных букв
+    guessed_words = []  # список уже названных слов
+    tries = 6  # количество попыток
+    display_hangman(tries)
+    print(word_completion)
+    input_1 = True
+    while input_1:
+        input_word = input('Input word or symbol >>> ')
+        out = test_symbol(input_word)
+        if out:
+            input_1 = False
+    print(input_word)
+
+
+play(get_word(word_list))
